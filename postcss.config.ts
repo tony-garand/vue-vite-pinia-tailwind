cf7e832b12d5360e31797f2482507290ba923dd5
tony-garand <anesthetics1@gmail.com>
2022-09-26 20:34:30 -0700

    fix(config): updating postcss

--- postcss.config.ts
+++
@@ -1,9 +0,0 @@
-export default  {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    'tailwindcss': {},
-    'autoprefixer': {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-  },
-}
